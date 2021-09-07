6e31e74c02b835618195d07e977aa8772493475a
Marcos Amorim <markstm10@gmail.com>
2021-09-07 00:33:47 -0300

    🛠 alterado componente header para funcionar com functions e typescript

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,29 @@
+import { Component } from "react";
+import { FiPlusSquare } from "react-icons/fi";
+
+import { Container } from "./styles";
+import Logo from "../../assets/logo.svg";
+
+interface HeaderProps {
+  openModal: () => void;
+}
+
+export function Header({ openModal }: HeaderProps) {
+  return (
+    <Container>
+      <header>
+        <img src={Logo} alt="GoRestaurant" />
+        <nav>
+          <div>
+            <button type="button" onClick={openModal}>
+              <div className="text">Novo Prato</div>
+              <div className="icon">
+                <FiPlusSquare size={24} />
+              </div>
+            </button>
+          </div>
+        </nav>
+      </header>
+    </Container>
+  );
+}
