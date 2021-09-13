a48f0e3648ed7d27529ccde9e99555ed342f2299
Marcos Amorim <markstm10@gmail.com>
2021-09-13 00:09:04 -0300

    🛠 migrado ModalEditFood para functions

--- src/components/ModalEditFood/index.jsx
+++ src/components/ModalEditFood/index.jsx
@@ -5,48 +5,37 @@ import { Form } from "./styles";
 import Modal from "../Modal";
 import { Input } from "../Input";
 
-class ModalEditFood extends Component {
-  constructor(props) {
-    super(props);
+export function ModalEditFood({
+  handleUpdateFood,
+  isOpen,
+  setIsOpen,
+  editingFood,
+}) {
+  const formRef = createRef();
 
-    this.formRef = createRef();
-  }
-
-  handleSubmit = async (data) => {
-    const { setIsOpen, handleUpdateFood } = this.props;
-
+  async function handleSubmit(data) {
     handleUpdateFood(data);
     setIsOpen();
-  };
-
-  render() {
-    const { isOpen, setIsOpen, editingFood } = this.props;
-
-    return (
-      <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-        <Form
-          ref={this.formRef}
-          onSubmit={this.handleSubmit}
-          initialData={editingFood}
-        >
-          <h1>Editar Prato</h1>
-          <Input name="image" placeholder="Cole o link aqui" />
-
-          <Input name="name" placeholder="Ex: Moda Italiana" />
-          <Input name="price" placeholder="Ex: 19.90" />
-
-          <Input name="description" placeholder="Descrição" />
-
-          <button type="submit" data-testid="edit-food-button">
-            <div className="text">Editar Prato</div>
-            <div className="icon">
-              <FiCheckSquare size={24} />
-            </div>
-          </button>
-        </Form>
-      </Modal>
-    );
   }
-}
 
-export default ModalEditFood;
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+}
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -4,7 +4,7 @@ import { Header } from "../../components/Header";
 import api from "../../services/api";
 import { Food } from "../../components/Food";
 import { ModalAddFood } from "../../components/ModalAddFood";
-import ModalEditFood from "../../components/ModalEditFood";
+import { ModalEditFood } from "../../components/ModalEditFood";
 import { FoodsContainer } from "./styles";
 
 class Dashboard extends Component {
